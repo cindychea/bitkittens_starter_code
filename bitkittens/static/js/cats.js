f202304d5de382b6cf9083adc8ecba607558bb9f
cindychea <cindy_chea@hotmail.com>
2019-06-20 15:50:30 -0400

    completed bitkittens exercise

--- bitkittens/static/js/cats.js
+++ bitkittens/static/js/cats.js
@@ -1,2 +1,23 @@
 document.addEventListener("DOMContentLoaded", function() {
+
+    const summonCats = document.querySelector('.summon-cats');
+
+    summonCats.addEventListener('click', function(e) {
+        axios.get('http://bitkittens.herokuapp.com/cats.json')
+        .then(response => {
+            let catData = response.data.cats
+            catData.forEach(function (element, index) {
+                let catPic = document.createElement('img')
+                catPic.src = element.photo
+                catPic.alt = 'Photo of ' + element.name
+                const catDiv = document.getElementsByClassName('cat-box')
+                catDiv[index].innerText = ''
+                catDiv[index].append(catPic)
+            });
+        })
+        .catch(error => {
+            console.log(error)
+        })
+    });
+
 });
