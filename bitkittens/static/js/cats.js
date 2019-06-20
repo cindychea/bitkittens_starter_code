a376d601d21694fba7e425947fc30d58fb1ca0cb
cindychea <cindy_chea@hotmail.com>
2019-06-20 16:03:36 -0400

    added additional cats to yard and added param to AJAX call

--- bitkittens/static/js/cats.js
+++ bitkittens/static/js/cats.js
@@ -3,20 +3,22 @@ document.addEventListener("DOMContentLoaded", function() {
     const summonCats = document.querySelector('.summon-cats');
 
     summonCats.addEventListener('click', function(e) {
-        axios.get('http://bitkittens.herokuapp.com/cats.json')
+        axios.get('http://bitkittens.herokuapp.com/cats.json', {
+            params: {number: 5}
+        })
         .then(response => {
-            let catData = response.data.cats
+            let catData = response.data.cats;
             catData.forEach(function (element, index) {
-                let catPic = document.createElement('img')
-                catPic.src = element.photo
-                catPic.alt = 'Photo of ' + element.name
-                const catDiv = document.getElementsByClassName('cat-box')
-                catDiv[index].innerText = ''
-                catDiv[index].append(catPic)
+                let catPic = document.createElement('img');
+                catPic.src = element.photo;
+                catPic.alt = 'Photo of ' + element.name;
+                const catDiv = document.getElementsByClassName('cat-box');
+                catDiv[index].innerText = '';
+                catDiv[index].append(catPic);
             });
         })
         .catch(error => {
-            console.log(error)
+            console.log(error);
         })
     });
 
